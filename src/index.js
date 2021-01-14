b315f8a5599e961a9074345294d4f42222996939
kam3r0nz <kam.hungerford@gmail.com>
2021-01-14 00:42:53 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here	  const form = document.querySelector('form');
+
+  let form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+
+  })
 });
