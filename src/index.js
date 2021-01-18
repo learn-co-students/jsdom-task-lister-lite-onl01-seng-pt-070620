8a4815c5b8a0d4a6fdf616223fb036cae3ea2ba0
julieconnors <juliana.g.connors@gmail.com>
2021-01-17 21:47:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+const info = document.getElementById("new-task-description");
+const input = document.querySelector('input');
+
+input.addEventListener('input', function(e) {
+  info.textContent = e.target.value
 });
+
+form.addEventListener("submit", function(event) {
+  console.log(info)
+  event.preventDefault();
+  let task = info.textContent
+  console.log(task)
+}
+);
+
+document.addEventListener("DOMContentLoaded", function() => {
+  // your code here
+  
+
+  
+
+  
+
+
+});
+
+
+
+let list = document.getElementById("list")
+let l = document.createElement("ul")
+
+const input = document.querySelector('input')
+
+
