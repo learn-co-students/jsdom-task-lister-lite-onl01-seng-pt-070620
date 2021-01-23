ca494d6623efbd71fcf66fc827e9f35b1a333f25
guerino antonini <guerantonini94@gmail.com>
2021-01-23 16:43:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // Handle the form submit event (when submit button is clicked)
+  //  - prevent default form action
+  //  - grab the text from the input box
+  //  - create a li element with the text
+  //  - add li to the ul
+  
+  const form = document.getElementById("create-task-form");
+  const todoInput = document.getElementById("new-task-description");
+  const todoList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let todoText = todoInput.value;
+    let li = document.createElement("li");
+    li.innerText = todoText;
+    todoList.appendChild(li);
+  });
 });
